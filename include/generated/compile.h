7b2b7752ff6d4a003e5ca11f935b72e2b70bd4c0
Raza <raza.tiana@gmail.com>
2015-03-16 13:37:08 +0100

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Fri Feb 6 17:41:53 CET 2015"
+#define LINUX_COMPILE_BY "razaina"
+#define LINUX_COMPILE_HOST "kali"
+#define LINUX_COMPILER "gcc version 4.9 20140827 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.64-CM-gc426a2f"
